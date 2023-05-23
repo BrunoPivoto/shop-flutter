992717560b2f759bc173c46f9dcd7e6c67106ffb
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-05-23 13:09:32 -0300

    edit product screen

---
+++ lib/components/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      title: Text(product.name),
+      //subtitle: Text(product.description),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.edit),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/models/product_list.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -18,7 +19,14 @@ class ProductsScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: products.itemsCount,
-          itemBuilder: (context, index) => Text(products.items[index].name),
+          itemBuilder: (context, index) => Column(
+            children: [
+              ProductItem(
+                product: products.items[index],
+              ),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
