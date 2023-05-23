2271de6080b1aa28b1a4b5b3cb14b9fdc76dfd6e
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-05-23 13:00:55 -0300

    add edit products screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/screens/cart_screen.dart';
 import 'package:shop/screens/orders_screen.dart';
 import 'package:shop/screens/product_detail_screen.dart';
+import 'package:shop/screens/products_screen.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 import 'package:shop/utils/app_route.dart';
 
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailScreen(),
           AppRoutes.CART: (ctx) => const CartScreen(),
           AppRoutes.ORDERS: (ctx) => const OrdersScreen(),
+          AppRoutes.PRODUCTS: (ctx) => const ProductsScreen(),
         },
       ),
     );
